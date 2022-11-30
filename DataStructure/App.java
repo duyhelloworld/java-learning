a4c033616b72dfd662ceed4bea9f0716d41d83ee
duyhelloworld <codedaovoiduy@gmail.com>
2022-11-30 17:48:20 +0700

    new file:   DataStructure/App.java 	new file:   DataStructure/BinarySearch.java 	new file:   DataStructure/LinkedList.java

---
+++ DataStructure/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.init();
+        // System.out.println(list);
+        // System.out.println(list.isEmpty());
+        list.display();
+    }
+}
